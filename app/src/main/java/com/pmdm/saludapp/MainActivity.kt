a89206e969770783bb691d93acfe8b286ae5aa8a
David de Andrés <davdeafon@alu.edu.gva.es>
2023-11-07 12:56:41 +0100

    Finalizado

--- app/src/main/java/com/pmdm/saludapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludapp/MainActivity.kt
@@ -1,17 +1,41 @@
 package com.pmdm.saludapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        val btnHello: AppCompatButton = findViewById(R.id.btnHello)
+        val etName: AppCompatEditText = findViewById(R.id.etName)
+
         btnHello.setOnClickListener {
-            Log.i("Test", "Botón pulsado")
+            val name = etName.text.toString()
+//            Log.i("Test", "Botón pulsado")
+//            Toast.makeText(this, "Hola $name!",
+//            Toast.LENGTH_LONG).show()
+            if (name.isNotEmpty()) {
+                Snackbar.make(btnHello, "Hola $name!", Snackbar.LENGTH_SHORT)
+                    .setAnchorView(etName)
+                    //.setAction("CLOSE") { finishAffinity() } //Cerrar app
+                    //.setAction("RESET") { etName.setText(null) } //Borrar texto
+                    .setAction("NEXT SCREEN") {
+                        val intentGA = Intent(this, GreetingActivity::class.java)
+                        intentGA.putExtra("EXTRA_NAME", name)
+                        startActivity(intentGA)
+                    }
+                    .show()
+            } else {
+                Toast.makeText(this, "Introduce un nombre", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
